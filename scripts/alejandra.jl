149c630c6a6495a1e0df83d573c1d90eee3f30cd
Matteo Smerlak <matteo.smerlak@gmail.com>
2022-03-23 16:17:05 +0100

    update to julia 1.7

--- scripts/alejandra.jl
+++ scripts/alejandra.jl
@@ -1,16 +1,16 @@
 using Distributed
 
-using Dates
-using DataFrames, StatsPlots, Arrow, DataFramesMeta
-using Plots
-
-
-PROCESSES = Sys.CPU_THREADS
+PROCESSES = Sys.CPU_THREADS - 1
 addprocs(PROCESSES)
-
 
 @everywhere using DrWatson
 @everywhere @quickactivate "TD"
+
+
+
+using Dates
+using DataFrames, StatsPlots, Arrow, DataFramesMeta
+using Plots
 
 
 @everywhere begin
@@ -19,8 +19,8 @@ addprocs(PROCESSES)
 end
 
 
-@everywhere function initialize(; n, μ, δ, init_strategy = 50)
-    return create_model(Dict{Symbol, Any}(:n => n, :μ => μ, :δ => δ, :init_strategy => init_strategy))
+@everywhere function initialize(; n, μ, δ, init_strategy = 50, σ = 1.)
+    return create_model(Dict{Symbol, Any}(:n => n, :μ => μ, :δ => δ, :init_strategy => init_strategy, :σ => σ))
 end
 
 ### time series
@@ -55,7 +55,7 @@ savefig(current(), plotsdir("vary_mu.png"))
 ### phase diagram
 
 params = Dict(  :n => 100, 
-                :μ => collect(1e-2:5e-3:2e-1), 
+                :μ => collect(1e-2:5e-3:4e-1), 
                 :δ => collect(1:20)
             )
 
@@ -80,8 +80,11 @@ Arrow.write(datadir(string(today()), "vary_mu_delta.arrow"), adata)
 
 scatter(
     adata[adata.step .== 1000, :μ], 
-    adata[adata.step .== 1000, :δ], 
-    marker_z = adata[adata.step .== 1000, :mean_strategy], label = "mean claim at t = 1000",
+    adata[adata.step .== 1000, :δ],
+    title = "reward = $REWARD, punishment = $PUNISHMENT",
+    legend = true,
+    marker_z = adata[adata.step .== 1000, :mean_strategy], 
+    label = "mean claim at t = 1000",
     markershape = :rect,
     markersize = 10,
     xlabel = "mutation probability μ",
@@ -90,4 +93,37 @@ scatter(
     )
 
 savefig(plotsdir("phase-diagram"))
+
+
+## vary selection strength
+
+
+### time series
+
+params = Dict(  :n => 100, 
+                :μ => 1e-1, 
+                :δ => 10,
+                :σ => collect(.01:.05:.4)
+            )
+
+adata, _ = paramscan(params, initialize; 
+    agent_step! = player_step!, 
+    model_step! = WF_sampling!, 
+    n = 1_000, 
+    adata = [(:strategy, mean)], 
+    parallel = true
+    )
+
+@df adata plot(:step, :mean_strategy, group = (:σ), legend = :bottomright, xlabel = "generation", ylabel = "mean claim", dpi = 300)
+
+try
+    mkdir(datadir(string(today())))
+catch
+    @warn "today's directory already exists"
+end
+
+
+Arrow.write(datadir(string(today()), "vary_sigma.arrow"), adata)
+
+savefig(current(), plotsdir("vary_mu.png"))
 
